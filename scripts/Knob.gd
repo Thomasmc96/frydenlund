7ec4c51e74fb59d9aa107745fb8ced6eaa205e1c
Thomasmc96 <tns@hiper.dk>
2025-02-15 09:26:52 +0100

    smoother camera

---
+++ scripts/Knob.gd
@@ -0,0 +1,43 @@
+extends Sprite2D
+@onready var parent = $".."
+
+
+var pressing = false
+
+@export var maxLength = 1000 #adjust?
+var deadzone = 10 #adjust?
+
+
+func _ready():
+	maxLength *= parent.scale.x
+
+func _process(delta):
+	if pressing:
+		if get_global_mouse_position().distance_to(parent.global_position) <= maxLength:
+			global_position = get_global_mouse_position()
+		else:
+			var angle = parent.global_position.angle_to_point(get_global_mouse_position())
+			global_position.x = parent.global_position.x + cos(angle)*maxLength
+			global_position.y = parent.global_position.y + sin(angle)*maxLength
+		calculateVector()
+	else:
+		global_position = lerp(global_position, parent.global_position, delta*50)
+		parent.posVector = Vector2(0,0)
+		
+func calculateVector():
+	if abs((global_position.x - parent.global_position.x)) >= deadzone:
+		parent.posVector.x = (global_position.x - parent.global_position.x)/maxLength
+	else:
+		parent.posVector.x = 0
+	if abs((global_position.y - parent.global_position.y)) >= deadzone:
+		parent.posVector.y = (global_position.y - parent.global_position.y)/maxLength
+	else:
+		parent.posVector.y = 0
+		
+		
+func _on_button_button_down():
+	pressing = true
+
+
+func _on_button_button_up():
+	pressing = false
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ class_name Player
 
 enum STATES {WALKING, IDLE}
 @onready var animated_sprite = %AnimatedSprite2D
+@onready var joystick = $Camera2D/Joystick
 
 const speed = 50
 var state = STATES.IDLE
@@ -12,29 +13,26 @@ var mouse_location = null
 
 func _physics_process(_delta):
 	mouse_location = get_global_mouse_position() - self.position
-	
-	var direction = Input.get_vector("left", "right", "up", "down")
-	if direction.x == 0 and direction.y == 0:
-		state = STATES.IDLE
-	elif direction.x != 0 or direction.y != 0:
-		state = STATES.WALKING
-	
-	velocity = direction * speed
-	move_and_slide()
-	
-	play_animation(direction)
+
+	if (OS.has_feature('web_android') || OS.has_feature('web_ios')):
+		joystick_movement()		
+	else:
+		keyboard_movement()
+		joystick.visible = false
+
+
 	
 func play_animation(direction: Vector2):
 	if state == STATES.IDLE:
 			animated_sprite.play("idle")
 	elif state == STATES.WALKING:
-		if direction.y == -1:
+		if direction.y < -0.9:
 			animated_sprite.play("walk_n")
-		if direction.x == 1:
+		if direction.x > 0.9:
 			animated_sprite.play("walk_e")
-		if direction.y == 1:
+		if direction.y > 0.9:
 			animated_sprite.play("walk_s")
-		if direction.x == -1:
+		if direction.x < -0.9:
 			animated_sprite.play("walk_w")
 			
 		if direction.x > 0.5 and direction.y < -0.5:
@@ -45,3 +43,31 @@ func play_animation(direction: Vector2):
 			animated_sprite.play("walk_sw")
 		if direction.x < -0.5 and direction.y < -0.5:
 			animated_sprite.play("walk_nw")
+
+
+func keyboard_movement():
+	var direction = Input.get_vector("left", "right", "up", "down")
+	if direction.x == 0 and direction.y == 0:
+		state = STATES.IDLE
+	elif direction.x != 0 or direction.y != 0:
+		state = STATES.WALKING
+	
+	velocity = direction.normalized() * speed
+	move_and_slide()
+	play_animation(direction)
+	
+func joystick_movement():
+	var direction = joystick.posVector
+	if direction.x == 0 and direction.y == 0:
+		state = STATES.IDLE
+	elif direction.x != 0 or direction.y != 0:
+		state = STATES.WALKING
+		
+	if direction:
+		velocity = direction * speed
+	else:
+		velocity = Vector2(0,0)
+	
+	move_and_slide()
+	play_animation(direction)
+	
