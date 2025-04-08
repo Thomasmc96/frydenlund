5a5e8ecde4e4ae7ca0eeffdd395891ab34551d91
Thomasmc96 <tns@hiper.dk>
2025-04-08 07:53:51 +0200

    flagpole

--- build/index.service.worker.js
+++ build/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1744007096|3226978531";
+const CACHE_VERSION = "1744091605|1836690578";
 const CACHE_PREFIX = "Frydenlund-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
